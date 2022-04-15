0cfe8b7ad6fde8122f7b0e7c41463d5a057d287a
SanamVahabzada <senem.vahabzade@gmail.com>
2022-04-15 12:25:27 +0400

    next changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
 let add = document.querySelector('button');
 let list = document.querySelector('.list');
 let input = document.querySelector('input');
-
-let filter = document.querySelector('.filterIcon')
-
-
+let filter = document.querySelector('.filterIcon');
 let remove = document.querySelector('.removeIcon');
+let error = document.querySelector('.error');
 
 
 
@@ -49,30 +47,44 @@ add.addEventListener('click', () => {
     let p = document.createElement('p');
     p.className = "list-item";
     let text = document.createTextNode(input.value);
-    p.append(text);
+    let span = document.createElement('span');
+    let text2 = document.createTextNode('\u2716');
+    span.className = 'delete'
+
 
     if (input.value === '') {
-        alert("You must write something!");
+        error.style.display = 'block';
     } else {
-    list.append(p);
-    list.style.display = 'block';
+        error.style.display = 'none';
+        list.style.display = 'block';
+        p.append(text);
+        list.append(p);
+        span.append(text2);
+        list.append(p);
+        p.append(span);
     }
-
     input.value = "";
-    let span = document.createElement('span');
-    let text2 = document.createTextNode('\u00D7');
-    span.className = 'delete'
-    span.append(text2);
-    p.append(span);
-    list.append(p);
-
-    let listItems = document.querySelectorAll('.list-item');
 
     span.addEventListener('click', () => {
         p.remove();
-
-        if (listItems.length == 1) {
+        if (list.innerHTML == '') {
             list.style.display = 'none';
         }
     });
 });
+
+function sort () {
+    let listItems = document.querySelectorAll('.list-item');
+    let arr = [];
+    listItems.forEach(items => {
+        arr.push(items.innerText);
+    });
+    arr.sort() 
+    arr.forEach ((item,i)=> {
+        listItems[i].innerText=item;
+    })
+
+
+}
+
+filter.addEventListener('click', sort);
