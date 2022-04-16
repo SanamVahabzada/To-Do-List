0549224e2ca05b130c00e85ee1e76967cf11cd77
SanamVahabzada <senem.vahabzade@gmail.com>
2022-04-17 00:02:23 +0400

    next changes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let inputPart = document.querySelector('.inputPart');
 let filter = document.querySelector('.filterIcon');
 let remove = document.querySelector('.removeIcon');
 let error = document.querySelector('.error');
+let sortIcon = document.querySelector('#sort');
 
 
 
@@ -48,17 +49,45 @@ remove.addEventListener('click', () => {
 
 add.addEventListener('click', create);
 
+
+let a = 0;
 function sort() {
+    a++;
     let listItems = document.querySelectorAll('.list-item');
     let arr = [];
     listItems.forEach(items => {
         arr.push(items.innerText);
     });
-    if (list.innerHTML !== '') {
+    if (a === 1) {
         arr.sort()
         arr.forEach((item, i) => {
             listItems[i].innerText = item;
         });
-    } 
+        filter.style.display = 'none'
+        sortIcon.style.display = 'block';
+        inputPart.style.display = 'none';
+
+    } else if (a === 2) {
+        arr.sort().reverse();
+        arr.forEach((item, i) => {
+            listItems[i].innerText = item;
+        });
+
+        sortIcon.style.display = 'none';
+        filter.style.display = 'block';
+        inputPart.style.display = 'none';
+    }
+
+    else {
+        arr.sort()
+        arr.forEach((item, i) => {
+            listItems[i].innerText = item;
+        });
+        filter.style.display = 'none'
+        sortIcon.style.display = 'block';
+        inputPart.style.display = 'block';
+        a = 0;
+    }
 }
 filter.addEventListener('click', sort);
+sortIcon.addEventListener('click', sort);
